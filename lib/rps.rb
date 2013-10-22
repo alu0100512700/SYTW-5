e92b56e5656cd1666f25f6c9acddd0ebb5dfb559
jonathan <jonathan@JHP-VirtualBox.(none)>
2013-10-22 21:12:16 +0100

    Desarrollada sesiones-cookies

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,6 +1,7 @@
 require 'rack/request'
 require 'rack/response'
 require 'haml'
+require 'rack'
 
 module RockPaperScissors
     class App 
@@ -15,8 +16,51 @@ module RockPaperScissors
         }.join("\n")
         @choose = "<p>\n<ul>\n#{@choose}\n</ul>\n</p>"				#Puedes añadir en haml con %p 
       end
-  
+
+			def set_env(env)
+				@env = env
+				@session = env['rack.session']
+			end
+
+			def win
+				return @session['won'].to_i if @session['won']
+				@session['won'] = 0
+			end
+
+			def win=(value)
+				@session['won'] = value
+			end
+
+			def lose
+				return @session['lost'].to_i if @session['lost']
+				@session['lost'] = 0
+			end
+
+			def lose=(value)
+				@session['lost'] = value
+			end
+
+			def tie
+				return @session['tied'].to_i if @session['tied']
+				@session['tied'] = 0
+			end
+
+			def tie=(value)
+				@session['tied'] = value
+			end
+
+			def play
+				return @session['play'].to_i if @session['play']
+				@session['play'] = 0
+			end
+
+			def play=(value)
+				@session['play'] = value
+			end
+
       def call(env)
+
+				set_env(env)
         req = Rack::Request.new(env)
   
         req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }
@@ -26,11 +70,23 @@ module RockPaperScissors
         answer = if !@throws.include?(player_throw)
             ""
           elsif player_throw == computer_throw
+						
+						self.play=self.play+1
+						self.tie=self.tie+1
             "Result: You tied with the computer"
+						
           elsif computer_throw == @defeat[player_throw]
-            "Result: Nicely done; #{player_throw} beats #{computer_throw}"
+            
+						self.play=self.play+1
+						self.win=self.win+1
+						"Result: Nicely done; #{player_throw} beats #{computer_throw}"
+						
           else
+						
+						self.play=self.play+1
+						self.lose=self.lose+1
             "Result: Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
+						
           end
 
         engine = Haml::Engine.new File.open("views/index.haml").read  
@@ -39,6 +95,10 @@ module RockPaperScissors
         res.write engine.render({},
           :answer => answer,
           :choose => @choose,
+					:win => self.win,
+					:lose => self.lose,
+					:tie => self.tie,
+					:play => self.play,
           #:throws => @throws
           #:computer_throw => computer_throw,
           #:player_throw => player_throw,
@@ -49,13 +109,13 @@ module RockPaperScissors
     end   # App
   end     # RockPaperScissors
 
-  if $0 == __FILE__
-    require 'rack/showexceptions'
-    Rack::Server.start(
-      :app => Rack::ShowExceptions.new(
-                Rack::Lint.new(
-                  RockPaperScissors::App.new)),
-      :Port => 9292,
-      :server => 'thin'
-    )
-  end
+#  if $0 == __FILE__
+#    require 'rack/showexceptions'
+#    Rack::Server.start(
+#      :app => Rack::ShowExceptions.new(
+#                Rack::Lint.new(
+#                  RockPaperScissors::App.new)),
+#      :Port => 9292,
+#      :server => 'thin'
+#    )
+#  end
