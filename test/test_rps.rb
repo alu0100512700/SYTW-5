efa6362e492cbdf493e09b47f0bfb0b97859056e
jonathan <jonathan@JHP-VirtualBox.(none)>
2013-10-23 20:11:57 +0100

    Añadidos algunos test

--- test/test_rps.rb
+++ test/test_rps.rb
@@ -1,21 +1,41 @@
 require "test/unit"
 require "rack/test"
-require "../lib/rps.rb"
+require "./lib/rps.rb"
 
 class RpsTest < Test::Unit::TestCase
 	include Rack::Test::Methods
-
+	
+	def initnav
+		@nav = Rack::Test::Session.new(Rack::MockSession.new(RockPaperScissors::App.new))
+	end
+	
 	def app
 		Rack::Builder.new do
-			run RockPaperScissors::App.new
-		end.to_app
+			run @nav
+		end
+			#run RockPaperScissors::App.new
+		#end.to_app
 	end
 
 	#############Añadir test
 
 	def test_index
-		get "/"
+		@nav.get "/"
 		#pust last_response.inspect
-		assert last_response.ok?
+		assert @nav.last_response.ok?
+	end
+
+	def computer
+		computer_throw = 'rock'
+	end
+
+	def test_tie
+		@nav.get"/?choice='rock'"
+		assert @nav.last_response.body.include?("Result: You tied with the computer")
+	end
+
+	def test_h2
+		@nav.get "/"
+		assert_match "<h2> Choose a throw: </h2>", @nav.last_response.body
 	end
 end
